5c7deb1576ddf1d6a51c4b2ef86e70cc6f76a30d
Brendt Bly <brendt.bly@outlook.com>
2018-12-19 14:46:44 -0600

    Get request, employee data displayed.

--- src/main/myapp/src/App.js
+++ src/main/myapp/src/App.js
@@ -1,24 +1,75 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
+const row = {
+  display: "flex",
+  flexFlow: "row wrap",
+  alignItems: "center",
+  justifyContent: "center",
+}
+
+const column = {
+  display: "flex",
+  flexFlow: "column nowrap",
+  alignItems: "center",
+  justifyContent: "center",
+}
+
+const cell = {
+  marginLeft: "10px",
+  width: "300px",
+}
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      employees: []
+    }
+  }
+
+  componentDidMount() {
+    // need env var for url in dev vs production?
+    const url = '/api/employees'
+    const self = this
+    axios.get(url, {
+      // params: {
+      //   id: id
+      // }
+    }).then(res => {
+      console.log('get employees action res.data');
+      console.log(res.data);
+      self.setState({
+        employees: res.data
+      })
+
+    }).catch(function (error) {
+      console.log('error get employees : ');
+      console.log(error);
+    });
+  }
   render() {
+    const employees = this.state.employees.map((employee, i) =>
+      <div style={row} key={i.toString()}>
+        <div style={cell}>{employee.firstName}</div>
+        <div style={cell}>{employee.lastName}</div>
+        <div style={cell}>{employee.email}</div>
+      </div>
+    )
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div style={column}>
+            <div style={row}>
+              <div style={cell}>First name</div>
+              <div style={cell}>Last name</div>
+              <div style={cell}>Email</div>
+            </div>
+            {employees}
+          </div>
         </header>
       </div>
     );
